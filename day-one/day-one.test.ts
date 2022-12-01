aa9ba9da5e85317a62ece76a11496008d34da7b1
Lockett Pundt <kalifornium@hotmail.com>
2022-12-01 12:46:38 -0500

    feat(AoC 2022): :sparkles: day one part 2 solution

--- day-one/day-one.test.ts
+++ day-one/day-one.test.ts
@@ -1,16 +1,21 @@
 import { splitByNewLine, splitByRegex } from '~/utils/split-by-new-line';
 import { describe, expect, test } from 'bun:test';
-import { dayOnePartOne } from './day-one';
+import { dayOnePartOne, dayOnePartTwo } from './day-one';
 
 describe(`day one tests`, () => {
-  test(`part one...`, () => {
-    const data = splitByRegex(
-      `test-data.txt`,
-      import.meta.dir,
-      new RegExp(/\n\n/, 'g')
-    );
+  const data = splitByRegex(
+    `test-data.txt`,
+    import.meta.dir,
+    new RegExp(/\n\n/, 'g')
+  );
 
+  test(`part one...`, () => {
     const result = dayOnePartOne(data);
     expect(result).toBe(24000);
+  });
+
+  test(`part two...`, () => {
+    const result = dayOnePartTwo(data);
+    expect(result).toBe(45000);
   });
 });
--- day-one/day-one.ts
+++ day-one/day-one.ts
@@ -14,3 +14,24 @@ export const dayOnePartOne = (data: string[]): number => {
     )
   );
 };
+
+export const dayOnePartTwo = (data: string[]): number => {
+  const sortedTotals = data
+    .reduce(
+      (elfCalorieTotals: number[], x: string) => [
+        ...elfCalorieTotals,
+        x
+          .split(/\n/g)
+          .reduce(
+            (total: number, calories: string) => (total += Number(calories)),
+            0
+          ),
+      ],
+      []
+    )
+    .sort((a, b) => b - a);
+
+  return sortedTotals
+    .slice(0, 3)
+    .reduce((total, current) => (total += current));
+};
