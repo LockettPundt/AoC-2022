e6f5859cdb5563f5315ff10903c4c30d5baed37e
Lockett Pundt <kalifornium@hotmail.com>
2022-12-01 12:49:47 -0500

    refactor(AoC 2022): :recycle: created function to get parsed totals

--- day-one/day-one.ts
+++ day-one/day-one.ts
@@ -1,35 +1,23 @@
-export const dayOnePartOne = (data: string[]): number => {
-  return Math.max(
-    ...data.reduce(
-      (elfCalorieTotals: number[], x: string) => [
-        ...elfCalorieTotals,
-        x
-          .split(/\n/g)
-          .reduce(
-            (total: number, calories: string) => (total += Number(calories)),
-            0
-          ),
-      ],
-      []
-    )
+const getCalorieTotalsByElf = (data: string[]): number[] =>
+  data.reduce(
+    (elfCalorieTotals: number[], x: string) => [
+      ...elfCalorieTotals,
+      x
+        .split(/\n/g)
+        .reduce(
+          (total: number, calories: string) => (total += Number(calories)),
+          0
+        ),
+    ],
+    []
   );
+
+export const dayOnePartOne = (data: string[]): number => {
+  return Math.max(...getCalorieTotalsByElf(data));
 };
 
 export const dayOnePartTwo = (data: string[]): number => {
-  const sortedTotals = data
-    .reduce(
-      (elfCalorieTotals: number[], x: string) => [
-        ...elfCalorieTotals,
-        x
-          .split(/\n/g)
-          .reduce(
-            (total: number, calories: string) => (total += Number(calories)),
-            0
-          ),
-      ],
-      []
-    )
-    .sort((a, b) => b - a);
+  const sortedTotals = getCalorieTotalsByElf(data).sort((a, b) => b - a);
 
   return sortedTotals
     .slice(0, 3)
